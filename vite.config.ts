a9bc5f31946ae802e5ebf59badd980fd735f09b4
superman <1689851268@qq.com>
2023-04-15 23:16:38 +0800

    优化配置文件、添加页面 icon

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
     // 获取环境变量
     const envDir = process.cwd();
@@ -14,12 +12,8 @@ export default defineConfig(({ mode }) => {
     return {
         plugins: [
             vue(),
-            AutoImport({
-                // 自动导入 Element Plus 相关函数, 如: ElMessage, ElMessageBox... (带样式)
-                resolvers: [ElementPlusResolver()],
-            }),
             Components({
-                // 自动注册 Element Plus 相关组件
+                // 自动导入和注册 Element Plus 的函数和组件
                 resolvers: [ElementPlusResolver()],
             }),
         ],
@@ -38,6 +32,7 @@ export default defineConfig(({ mode }) => {
                     additionalData: '@import "@/styles/variable.scss";', // 全局引入 scss 变量
                 },
             },
+            devSourcemap: true,
         },
     };
 });
